66afd8da3f3c9a06421a8507235e594781795771
@Margarita52 <muzovatkinamargarita@gmail.com>
2025-11-10 01:51:24 +0300

    main.tsx

---
+++ testing-app/src/main.tsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+import "./styles/globals.css";
+
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "./styles/theme";
+import App from "./App";
+import "./styles/globals.css";
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+);
