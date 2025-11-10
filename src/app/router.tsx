35a6dae874525d2ba0ccb0b0bba77d6e6fedaedc
@Margarita52 <muzovatkinamargarita@gmail.com>
2025-11-11 01:02:59 +0300

    router.tsx

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -3,14 +3,17 @@ import AppLayout from "../layouts/AppLayout";
 import LoginPage from "../pages/login/LoginPage";
 import StudentPage from "../pages/student/StudentPage";
 import AdminPage from "../pages/admin/AdminPage";
+import NotFoundPage from "../pages/errors/NotFoundPage";
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <AppLayout />, // родитель с макетом и <Outlet />
+    element: <AppLayout />, 
+    errorElement: <NotFoundPage />, 
     children: [
       { path: "login", element: <LoginPage /> },
       { path: "student", element: <StudentPage /> },
       { path: "admin", element: <AdminPage /> },
+      { path: "*", element: <NotFoundPage /> },
     ],
   },
 ]);
